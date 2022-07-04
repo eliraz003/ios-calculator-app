3316545ce7f97a082c6b635b2949d0e9a42b6e6c
Eliraz Atia <elirazatia@Elirazs-Mac-mini.local>
2022-07-04 19:13:41 +0100

    User can now Long press the currency button in the UnitSelectionViewController to save and unsave it

--- calc/View Controllers/MenuViewController.swift
+++ calc/View Controllers/MenuViewController.swift
@@ -56,11 +56,13 @@ class UIScrollableSelector: UIView {
     }
 }
 
-class MenuButton: UIView {
+class MenuButton: UIButton {
     var label: String!
     var leftAccessory: UIView?
     var rightAccessory: UIView?
     var action: (() -> Void)!
+    
+    var configureMenu: () -> UIMenu? = { return nil }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
@@ -93,9 +95,18 @@ class MenuButton: UIView {
         border.rightAnchor.constraint(equalTo: rightAnchor, constant: -16).isActive = true
         border.backgroundColor = .white.withAlphaComponent(0.1)
         
-        self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
+        self.addTarget(self, action: #selector(tapped), for: .touchUpInside)
+        
+        self.isUserInteractionEnabled = true
+        self.isContextMenuInteractionEnabled = true
+        
         self.heightAnchor.constraint(equalToConstant: 46).isActive = true
-
+    }
+    
+    @discardableResult func configureMenuAs(_ configuration: @escaping () -> UIMenu?) -> MenuButton {
+        self.configureMenu = configuration
+        self.menu = configureMenu()
+        return self
     }
     
     convenience init(label: String, view: UIView, action: @escaping () -> Void) {
@@ -171,6 +182,8 @@ class UnitSelectionViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    
+    var unitButtons: [MenuButton] = []
     
     let unitView: UIView = {
         let view = UIView()
@@ -254,6 +267,7 @@ class UnitSelectionViewController: UIViewController {
         return units.map({ return $0.value as! Currency })
     }
 
+
     private func refreshScrollList() {
         view.layoutIfNeeded()
         scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: unitView.subviews.last?.frame.maxY ?? 0)
@@ -262,7 +276,7 @@ class UnitSelectionViewController: UIViewController {
     private func refreshCurrencyList() {
         let list = findItemsToShow()
         
-        unitView.subviews.forEach({ $0.removeFromSuperview() })
+        unitButtons.forEach({ $0.removeFromSuperview() })
         
         var previous: UIView?
         list.forEach(({ item in
@@ -275,13 +289,35 @@ class UnitSelectionViewController: UIViewController {
             label.layer.cornerRadius = 8
             label.layer.cornerCurve = .continuous
 
-            let button = MenuButton(label: item.name, view: label, action: { })
+            let button = MenuButton(label: item.name, view: label, action: {
+                print("Pressed", item.isoCode)
+            })
+                
+            func setConfigurationMenu() {
+                button.configureMenuAs {
+                    return UIMenu(title: item.isoCode, image: nil, identifier: nil, options: [], children: [
+                        (SavedCurrencyController.shared.savedCurrencies.contains(where: { return $0.value == true && $0.key == item.isoCode }))
+                            ? UIAction(title: "Remove " + item.isoCode + " From Favourites", image: nil, identifier: nil, discoverabilityTitle: nil, attributes: [], state: .off, handler: { _ in
+                                SavedCurrencyController.shared.save(currency: item, state: false)
+                                setConfigurationMenu()
+                            })
+                        
+                        : UIAction(title: "Set " + item.isoCode + " As Favourite", image: nil, identifier: nil, discoverabilityTitle: nil, attributes: [], state: .off, handler: { _ in
+                                SavedCurrencyController.shared.save(currency: item, state: true)
+                                setConfigurationMenu()
+                            })
+                    ])
+                }
+            }
+            
+            setConfigurationMenu()
             button.translatesAutoresizingMaskIntoConstraints = false
-            unitView.addSubview(button)
+            view.addSubview(button)
             button.topAnchor.constraint(equalTo: previous?.bottomAnchor ?? unitView.topAnchor, constant: (previous == nil) ? 0 : 4).isActive = true
             button.leftAnchor.constraint(equalTo: unitView.leftAnchor, constant: 0).isActive = true
             button.rightAnchor.constraint(equalTo: unitView.rightAnchor, constant: 0).isActive = true
             
+            unitButtons.append(button)
             previous = button
         }))
 
